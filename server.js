0b34435e4b86fc1a7e64fbef2fb9ce20eaf1f2cc
John Olamide <johnolamide22@gmail.com>
2020-06-12 09:14:03 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs  = require('fs');
+
+var app = express();
+
+// register the partials folder
+hbs.registerPartials(__dirname + '/views/partials');
+// tell express to use the hbs template
+app.set('view engine', 'hbs');
+// register an express middleware with app.use()
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method}: ${req.url}`;
+  console.log(log);
+  fs.appendFileSync('server.log', log + '\n');
+  next();
+});
+
+app.use((req, res, next) =>{
+  res.render('maintenance.hbs');
+});
+
+// register helper funtions
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) =>{
+  res.render('home.hbs',{
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my Website'
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request.'
+  });
+});
+
+app.listen(3000, () =>{
+  console.log('Server is up on port 3000');
+});
